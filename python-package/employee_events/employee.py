98bc2e50b53815503a7dcea9fadcdb2312a5c2a4
AlorainiDona <dh.aloraini@gmail.com>
2025-02-08 19:02:20 +0300

    Fix database column issue and other updates

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,46 +1,71 @@
 # Import the QueryBase class
-from employee_events.query_base import QueryBase
+# YOUR CODE HERE
+from .sql_execution import QueryMixin
+from .query_base import QueryBase
 
-# Import dependencies needed for SQL execution
+# Import dependencies needed for sql execution
 # from the `sql_execution` module
-from employee_events.sql_execution import QueryMixin
-import pandas as pd
+# YOUR CODE HERE
 
-# Define a subclass of QueryBase called Employee
-class Employee(QueryBase):
-    # Set the class attribute `name` to the string "employee"
+# Define a subclass of QueryBase
+# called Employee
+# YOUR CODE HERE
+
+
+class Employee(QueryBase, QueryMixin):
+    # Set the class attribute `name`
+    # to the string "employee"
+    # YOUR CODE HERE
     name = "employee"
 
-    # Define a method called `names` that receives no arguments
-    # This method should return a list of tuples from an SQL execution
+    # Define a method called `names`
+    # that receives no arguments
+    # This method should return a list of tuples
+    # from an sql execution
     def names(self):
-        # Query 3: Retrieve employees' full names and IDs
-        query = """
+        query_str = f"""
             SELECT first_name || ' ' || last_name AS full_name, employee_id
-            FROM employee;
-        """
-        return self.query(query)
-
-    # Define a method called `username` that receives an `id` argument
-    # This method should return a list of tuples from an SQL execution
-    def username(self, id):
-        # Query 4: Retrieve a specific employee's full name
-        query = f"""
-            SELECT first_name || ' ' || last_name AS full_name
-            FROM employee
-            WHERE employee_id = {id};
-        """
-        return self.query(query)
-
-    # Define a method called `model_data` that retrieves
-    # data for machine learning model predictions
-    def model_data(self, id):
-        query = f"""
-            SELECT SUM(positive_events) AS positive_events,
-                   SUM(negative_events) AS negative_events
             FROM {self.name}
-            JOIN employee_events
-                USING({self.name}_id)
-            WHERE {self.name}.{self.name}_id = {id}
         """
-        return self.pandas_query(query)
+        return self.query(query_str)
+
+    # Define a method called `username`
+    # that receives an `id` argument
+    # This method should return a list of tuples
+    # from an sql execution
+    def username(self, id):
+        query_str = f"""
+            SELECT first_name || ' ' || last_name AS full_name, employee_id
+            FROM {self.name}
+            WHERE employee_id = {id}
+        """
+        return self.query(query_str)
+    # YOUR CODE HERE
+
+        # Query 4
+        # Write an SQL query
+        # that selects an employees full name
+        # Use f-string formatting and a WHERE filter
+        # to only return the full name of the employee
+        # with an id equal to the id argument
+        # YOUR CODE HERE
+
+    # Below is method with an SQL query
+    # This SQL query generates the data needed for
+    # the machine learning model.
+    # Without editing the query, alter this method
+    # so when it is called, a pandas dataframe
+    # is returns containing the execution of
+    # the sql query
+    # YOUR CODE HERE
+
+    def model_data(self, id):
+        panda_query_str = f"""
+                    SELECT SUM(positive_events) positive_events
+                         , SUM(negative_events) negative_events
+                    FROM {self.name}
+                    JOIN employee_events
+                        USING({self.name}_id)
+                    WHERE {self.name}.{self.name}_id = {id}
+                """
+        return self.pandas_query(panda_query_str)
--- python-package/employee_events/sql_execution.py
+++ python-package/employee_events/sql_execution.py
@@ -5,7 +5,9 @@ import pandas as pd
 
 # Using pathlib, create a `db_path` variable
 # that points to the absolute path for the `employee_events.db` file
-db_path = Path(__file__).parent / "employee_events.db"
+#### YOUR CODE HERE
+db_path = Path(__file__).parent.absolute() / "employee_events.db"
+
 
 # OPTION 1: MIXIN
 # Define a class called `QueryMixin`
@@ -15,20 +17,19 @@ class QueryMixin:
     # that receives an sql query as a string
     # and returns the query's result
     # as a pandas dataframe
-    def pandas_query(self, sql_query: str) -> pd.DataFrame:
+    #### YOUR CODE HERE
+    def pandas_query(self, sql_query):
         with connect(db_path) as connection:
-            return pd.read_sql_query(sql_query, connection)
+            df = pd.read_sql_query(sql_query, connection)
+        return df
 
-    # Define a method named `query`
-    # that receives an sql_query as a string
-    # and returns the query's result as
-    # a list of tuples. (You will need
-    # to use an sqlite3 cursor)
-    def query(self, sql_query: str):
+    def query(self, sql_query):
         with connect(db_path) as connection:
             cursor = connection.cursor()
-            cursor.execute(sql_query)
-            return cursor.fetchall()
+            result = cursor.execute(sql_query).fetchall()
+        return result
+    
+
  
  # Leave this code unchanged
 def query(func):
