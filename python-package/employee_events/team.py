5d4582c0b8b875f4872c9e4f15b8d37473954932
AlorainiDona <dh.aloraini@gmail.com>
2025-02-08 18:13:44 +0300

    Fixed import path for QueryMixin in team.py

--- python-package/employee_events/team.py
+++ python-package/employee_events/team.py
@@ -1,44 +1,28 @@
 # Import the QueryBase class
 from employee_events.query_base import QueryBase
 
-
-# Import dependencies for sql execution
-from python-package.employee_events.sql_execution import QueryMixin
+# Import dependencies for SQL execution
+from employee_events.sql_execution import QueryMixin
 import pandas as pd
 
-# Create a subclass of QueryBase
-# called  `Team`
+# Create a subclass of QueryBase called `Team`
 class Team(QueryBase):
-    # Set the class attribute `name`
-    # to the string "team"
+    # Set the class attribute `name` to the string "team"
     name = "team"
 
-    # Define a `names` method
-    # that receives no arguments
-    # This method should return
-    # a list of tuples from an sql execution
+    # Define a `names` method that receives no arguments
+    # This method should return a list of tuples from an SQL execution
     def names(self):
-        # Query 5
-        # Write an SQL query that selects
-        # the team_name and team_id columns
-        # from the team table for all teams
-        # in the database
+        # Query 5: Retrieve all team names and IDs
         query = """
             SELECT team_name, team_id FROM team;
         """
         return self.query(query)
 
-    # Define a `username` method
-    # that receives an ID argument
-    # This method should return
-    # a list of tuples from an sql execution
+    # Define a `username` method that receives an ID argument
+    # This method should return a list of tuples from an SQL execution
     def username(self, id):
-        # Query 6
-        # Write an SQL query
-        # that selects the team_name column
-        # Use f-string formatting and a WHERE filter
-        # to only return the team name related to
-        # the ID argument
+        # Query 6: Retrieve a specific team name based on ID
         query = f"""
             SELECT team_name
             FROM team
@@ -46,19 +30,13 @@ class Team(QueryBase):
         """
         return self.query(query)
 
-    # Below is method with an SQL query
-    # This SQL query generates the data needed for
-    # the machine learning model.
-    # Without editing the query, alter this method
-    # so when it is called, a pandas dataframe
-    # is returned containing the execution of
-    # the sql query
+    # Define a `model_data` method for machine learning model predictions
     def model_data(self, id):
         query = f"""
             SELECT positive_events, negative_events FROM (
-                    SELECT employee_id
-                         , SUM(positive_events) positive_events
-                         , SUM(negative_events) negative_events
+                    SELECT employee_id,
+                           SUM(positive_events) AS positive_events,
+                           SUM(negative_events) AS negative_events
                     FROM {self.name}
                     JOIN employee_events
                         USING({self.name}_id)
